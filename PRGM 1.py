684090acedfcb6d402975f819b0b6809ea3613d6
Varshini-Gangatharan <130486054+Varshini-Gangatharan@users.noreply.github.com>
2023-11-27 14:52:27 +0530

    Add files via upload

---
+++ PRGM 1.py
@@ -0,0 +1,10 @@
+def iso():
+    a=len(s)
+    b=len(t)
+    if a==b:
+        print("True")
+    else:
+        print("False")
+s=input("Enter a string: ")
+t=input("Enter another string: ")
+iso()
